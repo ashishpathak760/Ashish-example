cc5c99dc962d3a8b24a162082b1144704a0f6d43
ashishpathak760 <102546103+ashishpathak760@users.noreply.github.com>
2022-03-30 20:01:31 +0530

    Add files via upload

---
+++ bashscripts/loop.sh
@@ -0,0 +1,22 @@
+#1bin/bash
+
+#for loops
+
+for i in 1 2 3 4 5
+
+do 
+   echo  "hello $i"
+   
+done   
+
+# for loops N number
+
+
+
+
+for i in {1..10}
+
+do 
+   echo  "say hello $i"
+   
+done   
