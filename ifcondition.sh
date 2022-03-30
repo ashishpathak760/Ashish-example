2d468be02f0a3aa8e6b8be78fd4239352638b7b6
ashishpathak760 <ashishpathak760@gmail.com>
2022-03-30 10:56:19 +0530

    my project

---
+++ ifcondition.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# string if/else condition
+
+read -p "enter your name : " name
+  
+if [ "$name" = "ashish" ]; 
+then
+
+     echo "You are awesome"
+else
+
+     echo "who are you ? "
+          
+fi
+
+
+# number if/else condition
+  
+
+         
+
+read -p "enter a number greater than 10 : " num
+
+
+if [ "$num" -gt 10 ];
+then 
+        echo "you are correct"
+        
+ else    
+        echo "you are incorrect"
+        
+fi                
+                 
+  
+  
+
